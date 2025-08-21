c315f43165eaf3c901184dae4389e97db9b5abcc
Haydeen16 <131599734+Haydeen16@users.noreply.github.com>
2025-08-21 05:47:24 -0700

    Create usePersistentState.ts

---
+++ lib/hooks/usePersistentState.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function usePersistentState<T>(key: string, initialValue: T) {
+  const [value, setValue] = useState<T>(() => {
+    if (typeof window !== 'undefined') {
+      const item = localStorage.getItem(key);
+      return item ? (JSON.parse(item) as T) : initialValue;
+    }
+    return initialValue;
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
+
+export default usePersistentState;
