908fed65432771169c6ef96764163d4431e01713
Haydeen16 <131599734+Haydeen16@users.noreply.github.com>
2025-08-21 05:44:54 -0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        '2xl': '1280px',
+      },
+    },
+    extend: {
+      colors: {
+        // custom colors if needed
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
